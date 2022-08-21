5ce9cb7c453898a117f7bdea12afe11a104b1b87
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-08-21 10:30:55 +0200

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,8 +24,10 @@ use Illuminate\Support\Facades\Route;
 //     return view('home.contact');
 // })->name('home.contact');
 
-Route::view('/', 'home.index')->name('home.index');
-Route::view('/contact', 'home.contact')->name('home.contact');
+Route::get('/', [HomeController::class, 'home'])->name('home.index');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
+
+Route::get('/single', AboutController::class);
 
 $posts = [
     1 => [
@@ -38,25 +43,27 @@ $posts = [
     ]
 ];
 
-Route::get('/posts', function () use ($posts) {
-    return view('posts.index', [
-        'posts' => $posts
-    ]);
-})->name('posts.index');
+Route::resource('/posts', PostsController::class)->only('index', 'show');
 
-Route::get('/posts/{id}', function ($id) use ($posts) {
+// Route::get('/posts', function () use ($posts) {
+//     return view('posts.index', [
+//         'posts' => $posts
+//     ]);
+// })->name('posts.index');
+
+// Route::get('/posts/{id}', function ($id) use ($posts) {
 
 
-    abort_if(!isset($posts[$id]), 404);
+//     abort_if(!isset($posts[$id]), 404);
 
-    return view('posts.show', [
-        'post' => $posts[$id]
-    ]);   
-})->name('posts.show');
-Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20)
-{
-    return 'Posts from ' . $daysAgo . ' days ago';
-})->name('posts.recent.index');
+//     return view('posts.show', [
+//         'post' => $posts[$id]
+//     ]);   
+// })->name('posts.show');
+// Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20)
+// {
+//     return 'Posts from ' . $daysAgo . ' days ago';
+// })->name('posts.recent.index');
 
 Route::get('/fun/responses', function () use ($posts) {
     return response($posts, 201)
